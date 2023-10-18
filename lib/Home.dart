893ea43a69bfbac5b6e9bf1907547b46c34e03bd
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-10-19 00:05:14 +0100

    added home

---
+++ lib/Home.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar
+        (title: Text("Home"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.home),
+            Text("HOME")
+          ],
+        ),
+      ),
+    );
+  }
+}
